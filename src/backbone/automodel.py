e909e01b3519c988fd292d2bed74065d6f0c55b7
Yair Schiff <yairschiff@gmail.com>
2025-05-07 19:55:37 -0400

    Enable model surgery on automodels

--- src/backbone/automodel.py
+++ src/backbone/automodel.py
@@ -25,6 +25,7 @@ class AutoModelFromScratch(nn.Module):
         ],
         pretrained_model_name_or_path: str,
         trust_remote_code: bool = True,
+        keep_every_n_layers: int = 1,
         **kwargs,
     ):
         super().__init__()
@@ -32,6 +33,12 @@ class AutoModelFromScratch(nn.Module):
             pretrained_model_name_or_path, trust_remote_code=trust_remote_code, **kwargs
         )
         self.model = AUTO_MODEL_CLS[automodel_cls].from_config(auto_config)
+        if keep_every_n_layers < len(self.model.layers):
+            layers_post_surgery = []
+            for i, layer in enumerate(self.model.layers):
+                if (i + 1) % keep_every_n_layers == 0:
+                    layers_post_surgery.append(layer)
+            self.model.layers = nn.ModuleList(layers_post_surgery)
 
     def forward(self, input_ids, **kwargs):
         return self.model(input_ids, **kwargs)
@@ -47,12 +54,19 @@ class AutoModelFromPreTrained(nn.Module):
         ],
         pretrained_model_name_or_path: str,
         trust_remote_code: bool = True,
+        keep_every_n_layers: int = 1,
         **kwargs,
     ):
         super().__init__()
         self.model = AUTO_MODEL_CLS[automodel_cls].from_pretrained(
             pretrained_model_name_or_path, trust_remote_code=trust_remote_code, **kwargs
         )
+        if keep_every_n_layers < len(self.model.layers):
+            layers_post_surgery = []
+            for i, layer in enumerate(self.model.layers):
+                if (i + 1) % keep_every_n_layers == 0:
+                    layers_post_surgery.append(layer)
+            self.model.layers = nn.ModuleList(layers_post_surgery)
 
     def forward(self, input_ids, **kwargs):
         return self.model(input_ids, **kwargs)
