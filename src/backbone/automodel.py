fb328fda7aecbabad2d4b4f9d7b776e129a1564f
Yair Schiff <yairschiff@gmail.com>
2025-05-07 20:14:24 -0400

    Add ar script

--- src/backbone/automodel.py
+++ src/backbone/automodel.py
@@ -61,12 +61,12 @@ class AutoModelFromPreTrained(nn.Module):
         self.model = AUTO_MODEL_CLS[automodel_cls].from_pretrained(
             pretrained_model_name_or_path, trust_remote_code=trust_remote_code, **kwargs
         )
-        if keep_every_n_layers < len(self.model.layers):
+        if keep_every_n_layers > 1:
             layers_post_surgery = []
-            for i, layer in enumerate(self.model.layers):
+            for i, layer in enumerate(self.model.model.layers):
                 if (i + 1) % keep_every_n_layers == 0:
                     layers_post_surgery.append(layer)
-            self.model.layers = nn.ModuleList(layers_post_surgery)
+            self.model.model.layers = nn.ModuleList(layers_post_surgery)
 
     def forward(self, input_ids, **kwargs):
         return self.model(input_ids, **kwargs)
