0dde55065821bd0a655bf3a762839b6abb67f535
Yair Schiff <yairschiff@gmail.com>
2025-05-09 15:59:12 -0400

    Add debug inference on train scripts

---
+++ g2_slurm_scripts/run_lm_eval_harness_debug.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#SBATCH -J lm_eval_harness            # Job name
+#SBATCH -o ../watch_folder/%x_%j.out  # Output file (%j expands to jobID)
+#SBATCH --get-user-env                # Retrieve the users login environment
+#SBATCH --partition=kuleshov               # Request partition
+#SBATCH --constraint="[a100|a6000|a5000|3090]"
+#SBATCH -t 960:00:00                  # Time limit (hh:mm:ss)
+#SBATCH --mem=64000                   # Server memory requested (per node)
+#SBATCH -N 1                          # Total number of nodes requested
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                  # Type/number of GPUs needed
+#SBATCH --open-mode=append            # Do not overwrite logs
+#SBATCH --requeue                     # Requeue upon preemption
+#SBATCH --mail-user=yzs2@cornell.edu  # Email
+#SBATCH --mail-type=END               # Request status by email
+
+# Setup environment
+cd ../ || exit  # Go to the root directory of the repo
+source setup_env.sh
+
+#MODEL_PATH="/home/ubuntu/runs/dllm-dev/gsm8k-block4-bs96-keep4-causalencfalse-max20000ba-lr1e-4-warmup1000ba-gc1.0-wd1e-5-bd3_phi_untie_v5"
+#MODEL_PATH="/home/ubuntu/test_ckpts"
+MODEL_PATH="/home/ubuntu/qwen3_600M_gsm8k_ckpts"
+#MODEL_PATH="microsoft/Phi-4-mini-reasoning"
+#MODEL_PATH="Qwen/Qwen3-0.6B-Base"
+OUTPUT_DIR="${MODEL_PATH}/lm_eval_harness_output"
+#OUTPUT_DIR="home/ubuntu/qwen3_lm_eval_harness_output"
+mkdir -p ${OUTPUT_DIR}
+L=128
+BLOCK_SIZE=4
+GREEDY=True
+USE_X0_PRED=True
+FIRST_HITTING=True
+LOW_CONFIDENCE_REMASKING=True
+KV_CACHING=True
+
+OUTPUT_PATH="${OUTPUT_DIR}/L=${L}-block_size=${BLOCK_SIZE}-greedy=${GREEDY}-use_x0_pred=${USE_X0_PRED}-first_hitting=${FIRST_HITTING}-low_confidence_remasking=${LOW_CONFIDENCE_REMASKING}"
+#OUTPUT_PATH="${OUTPUT_DIR}/outputs.json"
+
+python scripts/harness_eval_debug_train.py \
+  --tasks gsm8k \
+  --model lm_eval_harness_model \
+  --num_fewshot 0 \
+  --batch_size 1 \
+  --device cuda:0 \
+  --output_path ${OUTPUT_PATH} \
+  --model_args \
+    "max_cont_len=${L},\
+model_path=${MODEL_PATH},\
+load_ema_weights=True,\
+tokenizer_name_or_path=Qwen/Qwen3-0.6B-Base,\
+num_samples=1,\
+num_steps=8,\
+min_t=1e-5,\
+top_p=0.9,\
+pad_context=False,\
+greedy=${GREEDY},\
+use_x0_pred=${USE_X0_PRED},\
+first_hitting=${FIRST_HITTING},\
+low_confidence_remasking=${LOW_CONFIDENCE_REMASKING},\
+disable_cache=False,\
+kv_caching=${KV_CACHING},\
+max_length=768,\
+block_size=${BLOCK_SIZE},\
+shift_logits=True"
---
+++ scripts/harness_eval_debug_train.py
@@ -0,0 +1,233 @@
+"""
+This file is inspired by the code from https://github.com/ML-GSAI/SMDM
+"""
+
+import random
+from typing import List, Tuple
+
+import accelerate
+import numpy as np
+import torch
+from lm_eval.__main__ import cli_evaluate
+from lm_eval.api.model import LM
+from lm_eval.api.registry import register_model
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from scripts.utils import (
+    load_model_from_ckpt_dir_path,
+    maybe_add_missing_special_tokens,
+)
+from src.datasets.tokenize_on_demand import GSM8KDataset
+from src.sampler import SamplerConfig
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+@register_model("lm_eval_harness_model")
+class LMEvalHarness(LM):
+    def __init__(
+        self,
+        # Model args
+        max_cont_len: int = 128,
+        model_path: str = "",
+        tokenizer_name_or_path: str = "",
+        device: str = "cuda",
+        load_ema_weights: bool = True,
+        # Sampler args
+        num_samples: int = 1,
+        batch_size: int = 1,
+        num_steps: int = 1000,
+        min_t: float = 1e-5,
+        top_p: float = 0.9,
+        pad_context: bool = False,
+        greedy: bool = False,
+        use_x0_pred: bool = False,
+        first_hitting: bool = False,
+        low_confidence_remasking: bool = False,
+        disable_cache: bool = False,
+        kv_caching: bool = False,
+        max_length: int | None = None,  # Default to model config, if None
+        block_size: int | None = None,  # Default to model config, if None
+        shift_logits: bool | None = None,  # Default to model config, if None
+    ):
+        """
+        Args:
+            max_cont_len (int): Max length of continuation tokens.
+            model_path (str): Checkpoint path.
+            tokenizer_name_or_path (str): Tokenizer name or path.
+        """
+        super().__init__()
+        self.max_cont_length = max_cont_len
+        accelerator = accelerate.Accelerator()
+        if accelerator.num_processes > 1:
+            self.accelerator = accelerator
+        else:
+            self.accelerator = None
+
+        model_kwargs = {}
+        if self.accelerator is not None:
+            model_kwargs.update({"device_map": {"": f"{self.accelerator.device}"}})
+
+        self.device = torch.device(device)
+        self.tokenizer = maybe_add_missing_special_tokens(
+            AutoTokenizer.from_pretrained(
+                tokenizer_name_or_path, trust_remote_code=True
+            )
+        )
+        try:
+            self.model = load_model_from_ckpt_dir_path(
+                path_to_ckpt_dir=model_path,
+                load_ema_weights=load_ema_weights,
+            ).to(self.device)
+        except FileNotFoundError:
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_path,
+                trust_remote_code=True,
+            ).to(self.device)
+        self.model.eval()
+        # assert (
+        #     getattr(self.model, "mask_token_id", None) is not None
+        #     or getattr(self.tokenizer, "mask_token_id", None) is not None
+        # ), "Mask token id must be set in either the model or tokenizer."
+        self.mask_token_id = None
+        self.mask_token_id = getattr(
+            self.model, "mask_token_id", getattr(self.tokenizer, "mask_token_id", None)
+        )
+        self.sampler_config = SamplerConfig(
+            num_samples=num_samples,
+            batch_size=int(batch_size),
+            num_steps=num_steps,
+            min_t=min_t,
+            top_p=top_p,
+            pad_context=pad_context,
+            greedy=greedy,
+            use_x0_pred=use_x0_pred,
+            first_hitting=first_hitting,
+            low_confidence_remasking=low_confidence_remasking,
+            disable_cache=disable_cache,
+            kv_caching=kv_caching,
+            max_length=max_length
+            if max_length is not None
+            else self.model.config.length,
+            block_size=block_size
+            if block_size is not None
+            else self.model.config.block_size,
+            shift_logits=shift_logits
+            if shift_logits is not None
+            else self.model.config.shift_logits,
+        )
+        self.model.sampler_config = self.sampler_config
+        self.train_dataset = GSM8KDataset(
+            tokenizer=self.tokenizer,
+            split="train",
+            max_seq_len=self.model.config.length,
+        )
+
+    def loglikelihood(self, requests) -> List[Tuple[float, bool]]:
+        raise NotImplementedError
+
+    def loglikelihood_rolling(self, requests) -> List[float]:
+        raise NotImplementedError
+
+    def generate_until(self, requests, **generation_kwargs):
+        def _tokenize(
+            e,
+            prefix_text: str | None = (
+                "<|im_end|>Please reason step by step, and put your "
+                + "final answer within $\\boxed{}$. "
+            ),
+        ):
+            ctx = (prefix_text if prefix_text is not None else "") + e["prefix"]
+            # TODO: Hacks to make data look like training set
+            ctx = ctx.replace("Question: ", "")
+            ctx = ctx.replace("\nAnswer:", "<|im_end|>Answer:")
+            n_spaces = len(ctx) - len(ctx)
+            if n_spaces > 0:
+                ctx = ctx[:-n_spaces]
+            prefix_tokens = self.tokenizer(ctx)["input_ids"]
+            return {
+                "prefix_text": ctx,
+                "prefix": prefix_tokens,
+                "target": e["target"],
+            }
+
+        # ds = [{"prefix": req.args[0], "target": req.args[1]} for req in requests]
+        # ds = Dataset.from_list(ds)
+        # ds = ds.map(_tokenize)
+        # ds = ds.with_format("torch")
+        # total_len = [len(x["prefix"]) + self.max_cont_length for x in ds]
+        # assert max(total_len) <= self.sampler_config.max_length, (
+        #     "Input length(s) exceeds max_length"
+        # )
+        #
+        # res = []
+        # # res_for_json = []
+        del requests
+        correct, total = 0, 0
+        for elem in tqdm(self.train_dataset, desc="Generating"):
+            sample, _ = self.model.generate(
+                max_length=elem["context_mask"].sum() + self.max_cont_length,
+                context=elem["input_ids"][elem["context_mask"].bool()][None, ...].to(
+                    self.device
+                ),
+                device=self.device,
+                # tokenizer=self.tokenizer,  # For debugging
+            )
+            # sample = self.model.generate(
+            #     input_ids=elem["prefix"][None, ...].to(self.device),
+            #     max_length=len(elem["prefix"]) + self.max_cont_length,
+            #     num_return_sequences=1
+            # )
+            result = self.tokenizer.decode(sample[0])
+            question = result.split("Answer: ")[0]
+            result = result.split("Answer: ")[1]
+            # for until in elem["target"]["until"] + [
+            #     "<|eot_id|>",
+            #     self.tokenizer.eos_token,
+            # ]:
+            #     result = result.split(until)[0]
+            ground_truth = self.tokenizer.decode(
+                elem["input_ids"][~elem["context_mask"].bool()]
+            )
+            print("=" * 20)
+            print("Question: ", question)
+            print("\nAnswer: ", result)
+            print("\n(Ground truth): ", ground_truth)
+            print("=" * 20, end="\n\n")
+
+            # log accuracy
+            ground_truth_ans = ground_truth.split("\\boxed{")[1].split("}")[0]
+            if "\\boxed{" in result:
+                predicted_ans = result.split("\\boxed{")[1].split("}")[0]
+                if ground_truth_ans == predicted_ans:
+                    correct += 1
+            total += 1
+
+            # res_for_json.append(
+            #     {
+            #         "prefix": elem["prefix_text"],
+            #         "result": result,
+            #     }
+            # )
+            torch.cuda.empty_cache()
+            print(f"\nAccuracy: {correct}/{total} = {correct / total:.2%}\n")
+        # with open(self.model.config.eval.generated_samples_path, "w") as f:
+        #     json.dump(
+        #         res_for_json,
+        #         f,  # type: ignore
+        #         indent=2,
+        #     )
+        return []
+
+
+if __name__ == "__main__":
+    set_seed(1234)
+    cli_evaluate()
