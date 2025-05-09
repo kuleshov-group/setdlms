4383062c168db1c66b5dcc1fa3e86b6b19cb4ad2
Yair Schiff <yairschiff@gmail.com>
2025-05-08 20:31:23 -0400

    Update scripts

--- g2_slurm_scripts/run_lm_eval_harness.sh
+++ g2_slurm_scripts/run_lm_eval_harness.sh
@@ -18,8 +18,13 @@
 cd ../ || exit  # Go to the root directory of the repo
 source setup_env.sh
 
-MODEL_PATH="/home/ubuntu/runs/dllm-dev/gsm8k-block4-bs96-keep4-causalencfalse-max20000ba-lr1e-4-warmup1000ba-gc1.0-wd1e-5-bd3_phi_untie_v5"
+#MODEL_PATH="/home/ubuntu/runs/dllm-dev/gsm8k-block4-bs96-keep4-causalencfalse-max20000ba-lr1e-4-warmup1000ba-gc1.0-wd1e-5-bd3_phi_untie_v5"
+#MODEL_PATH="/home/ubuntu/test_ckpts"
+MODEL_PATH="/home/ubuntu/qwen3_600M_gsm8k_ckpts"
+#MODEL_PATH="microsoft/Phi-4-mini-reasoning"
+#MODEL_PATH="Qwen/Qwen3-0.6B-Base"
 OUTPUT_DIR="${MODEL_PATH}/lm_eval_harness_output"
+#OUTPUT_DIR="home/ubuntu/qwen3_lm_eval_harness_output"
 mkdir -p ${OUTPUT_DIR}
 L=128
 BLOCK_SIZE=4
@@ -29,7 +34,8 @@ FIRST_HITTING=True
 LOW_CONFIDENCE_REMASKING=True
 KV_CACHING=True
 
-OUTPUT_PATH="${OUTPUT_DIR}/L=${L}-block_size=${BLOCK_SIZE}-greedy=${GREEDY}-use_x0_pred=${USE_X0_PRED}-first_hitting=${FIRST_HITTING}-low_confidence_remasking=${LOW_CONFIDENCE_REMASKING}.json"
+OUTPUT_PATH="${OUTPUT_DIR}/L=${L}-block_size=${BLOCK_SIZE}-greedy=${GREEDY}-use_x0_pred=${USE_X0_PRED}-first_hitting=${FIRST_HITTING}-low_confidence_remasking=${LOW_CONFIDENCE_REMASKING}"
+#OUTPUT_PATH="${OUTPUT_DIR}/outputs.json"
 
 python scripts/harness_eval.py \
   --tasks gsm8k \
@@ -42,7 +48,7 @@ python scripts/harness_eval.py \
     "max_cont_len=${L},\
 model_path=${MODEL_PATH},\
 load_ema_weights=True,\
-tokenizer_name_or_path=microsoft/Phi-4-mini-reasoning,\
+tokenizer_name_or_path=Qwen/Qwen3-0.6B-Base,\
 num_samples=1,\
 num_steps=8,\
 min_t=1e-5,\
--- lambda_scripts/run_train_e2d2_gsm8k.sh
+++ lambda_scripts/run_train_e2d2_gsm8k.sh
@@ -6,24 +6,24 @@ NUM_VISIBLE_DEVICES=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 
 # Important variables (fix during hyperparam sweep)
 BLOCK_SIZE=4
-KEEP_EVERY_N_ENCODER_LAYERS=2
+KEEP_EVERY_N_ENCODER_LAYERS=1
 KEEP_EVERY_N_DECODER_LAYERS=2 # 2, 4
 USE_ENCODER_CAUSAL_MASK=false # true, false
 
 # Hyperparameters
-LR=1e-4 # 1e-5, 1e-4, 1e-3
+LR=1e-5 # 1e-5, 1e-4, 1e-3
 WARMUP_DURATION="1000ba" # 0.1, 0.3, 0.5
 BATCH_SIZE=96 # 96, 128, 256
 GRAD_CLIP=1.0 # 0.25, 0.5, 0.75, 1.0
 WEIGHT_DECAY=1e-5 # 1e-5, 1e-3
 MAX_DURATION="20000ba" # 20000ba, 10000ba, 5000ba
 
-PRETRAINED_MODEL_NAME_OR_PATH=microsoft/Phi-4-mini-reasoning # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
+PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-0.6B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
 
-TAG=bd3_phi_v1
+TAG=e2d2_qwen600M_v1
 RUN_NAME=gsm8k-block${BLOCK_SIZE}-bs${BATCH_SIZE}-keep${KEEP_EVERY_N_DECODER_LAYERS}-causalenc${USE_ENCODER_CAUSAL_MASK}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
 
-MICRO_BATCH_SIZE=2 # TODO: tune
+MICRO_BATCH_SIZE=6 # TODO: tune
 NUM_WORKERS=64 # TODO: tune
 
 composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoiser.py \
@@ -34,7 +34,7 @@ composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoi
   composer.optimizer.lr=${LR} \
   composer.optimizer.weight_decay=${WEIGHT_DECAY} \
   composer.algorithms.gradient_clipping.clipping_threshold=${GRAD_CLIP} \
-  composer.trainer.eval_interval='2ep' \
+  composer.trainer.eval_interval="4ep" \
   composer.trainer.max_duration=${MAX_DURATION} \
   composer.trainer.save_num_checkpoints_to_keep=1 \
   composer/lr_scheduler=cosine_annealing_with_warmup \
@@ -53,5 +53,6 @@ composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoi
   block_size=${BLOCK_SIZE} \
   training.antithetic_sampling=false \
   hydra.run.dir=/home/ubuntu/runs/dllm-dev/${RUN_NAME} \
-  composer.trainer.save_interval="2ep" \
-  composer.loggers.name=${RUN_NAME}
+  composer.trainer.save_interval="8ep" \
+  composer.loggers.name=${RUN_NAME} \
+  train_dataloader.num_workers=${NUM_WORKERS}
