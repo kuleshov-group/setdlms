178478e31a3569cd1f8c7d79418c2035d6499250
Yair Schiff <yairschiff@gmail.com>
2025-05-08 23:09:51 -0400

    Update to qwen 1.7B

--- lambda_scripts/run_train_e2d2_gsm8k.sh
+++ lambda_scripts/run_train_e2d2_gsm8k.sh
@@ -18,13 +18,13 @@ GRAD_CLIP=1.0 # 0.25, 0.5, 0.75, 1.0
 WEIGHT_DECAY=1e-5 # 1e-5, 1e-3
 MAX_DURATION="20000ba" # 20000ba, 10000ba, 5000ba
 
-PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-0.6B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
+PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-1.7B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
 
-TAG=e2d2_qwen600M_v1
+TAG=e2d2_qwen2B_v1
 RUN_NAME=gsm8k-block${BLOCK_SIZE}-bs${BATCH_SIZE}-keep${KEEP_EVERY_N_DECODER_LAYERS}-causalenc${USE_ENCODER_CAUSAL_MASK}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
 
-MICRO_BATCH_SIZE=6 # TODO: tune
-NUM_WORKERS=64 # TODO: tune
+MICRO_BATCH_SIZE=2 # TODO: tune
+NUM_WORKERS=128 # TODO: tune
 
 composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoiser.py \
   run_name=${RUN_NAME} \
