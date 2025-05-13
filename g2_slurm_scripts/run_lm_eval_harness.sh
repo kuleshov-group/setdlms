2d9479e4e6a34f4fece5b62b74e7978ddb09237a
Yair Schiff <yairschiff@gmail.com>
2025-05-13 14:42:38 +0000

    Edit params

--- g2_slurm_scripts/run_lm_eval_harness.sh
+++ g2_slurm_scripts/run_lm_eval_harness.sh
@@ -18,9 +18,10 @@
 cd ../ || exit  # Go to the root directory of the repo
 source setup_env.sh
 
+MODEL_PATH="/home/ubuntu/runs/dllm-dev/gsm8k-block4-bs96-keepbottom21-causalencfalse-max20000ba-lr1e-5-warmup1000ba-gc1.0-wd1e-5-bd3_small_qwen2B_v4"
 #MODEL_PATH="/home/ubuntu/runs/dllm-dev/gsm8k-block4-bs96-keep4-causalencfalse-max20000ba-lr1e-4-warmup1000ba-gc1.0-wd1e-5-bd3_phi_untie_v5"
 #MODEL_PATH="/home/ubuntu/test_ckpts"
-MODEL_PATH="/home/ubuntu/qwen3_600M_gsm8k_ckpts"
+#MODEL_PATH="/home/ubuntu/qwen3_600M_gsm8k_ckpts"
 #MODEL_PATH="microsoft/Phi-4-mini-reasoning"
 #MODEL_PATH="Qwen/Qwen3-0.6B-Base"
 OUTPUT_DIR="${MODEL_PATH}/lm_eval_harness_output"
@@ -28,17 +29,18 @@ OUTPUT_DIR="${MODEL_PATH}/lm_eval_harness_output"
 mkdir -p ${OUTPUT_DIR}
 L=256
 BLOCK_SIZE=4
-GREEDY=False
+GREEDY=True
 USE_X0_PRED=True
 FIRST_HITTING=True
 LOW_CONFIDENCE_REMASKING=True
-KV_CACHING=False
+KV_CACHING=True
 TOP_P=1.0
 
 OUTPUT_PATH="${OUTPUT_DIR}/L=${L}-block_size=${BLOCK_SIZE}-greedy=${GREEDY}-use_x0_pred=${USE_X0_PRED}-first_hitting=${FIRST_HITTING}-low_confidence_remasking=${LOW_CONFIDENCE_REMASKING}"
 #OUTPUT_PATH="${OUTPUT_DIR}/outputs.json"
 
-python scripts/harness_eval.py \
+#python scripts/harness_eval.py \
+accelerate launch scripts/harness_eval.py \
   --tasks gsm8k \
   --model lm_eval_harness_model \
   --num_fewshot 0 \
@@ -46,10 +48,11 @@ python scripts/harness_eval.py \
   --device cuda:0 \
   --output_path ${OUTPUT_PATH} \
   --model_args \
-    "max_cont_len=${L},\
+    "generated_samples_path=${OUTPUT_PATH},\
+max_cont_len=${L},\
 model_path=${MODEL_PATH},\
 load_ema_weights=False,\
-tokenizer_name_or_path=Qwen/Qwen3-0.6B-Base,\
+tokenizer_name_or_path=Qwen/Qwen3-1.7B-Base,\
 num_samples=1,\
 num_steps=8,\
 min_t=1e-5,\
