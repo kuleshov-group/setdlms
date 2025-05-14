c3e11b1fa6bb079122858f563a2aa2069f573f9a
mariannearr <marianne.arriola@gmail.com>
2025-05-14 19:13:02 +0000

    updated gsm8k with most recent config

--- lambda_scripts/run_train_e2d2_gsm8k.sh
+++ lambda_scripts/run_train_e2d2_gsm8k.sh
@@ -7,8 +7,9 @@ NUM_VISIBLE_DEVICES=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 # Important variables (fix during hyperparam sweep)
 BLOCK_SIZE=4
 KEEP_EVERY_N_ENCODER_LAYERS=1
-KEEP_EVERY_N_DECODER_LAYERS=2 # 2, 4
+KEEP_EVERY_N_DECODER_LAYERS=1 # 2, 4
 USE_ENCODER_CAUSAL_MASK=false # true, false
+KEEP_TOP_N_DECODER_LAYERS=28
 
 # Hyperparameters
 LR=1e-5 # 1e-5, 1e-4, 1e-3
@@ -20,10 +21,10 @@ MAX_DURATION="20000ba" # 20000ba, 10000ba, 5000ba
 
 PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-1.7B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
 
-TAG=e2d2_qwen2B_v1
-RUN_NAME=gsm8k-block${BLOCK_SIZE}-bs${BATCH_SIZE}-keep${KEEP_EVERY_N_DECODER_LAYERS}-causalenc${USE_ENCODER_CAUSAL_MASK}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
+TAG=e2d2_qwen2B_tie_v1
+RUN_NAME=gsm8k-block${BLOCK_SIZE}-bs${BATCH_SIZE}-keeptop${KEEP_TOP_N_DECODER_LAYERS}-causalenc${USE_ENCODER_CAUSAL_MASK}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
 
-MICRO_BATCH_SIZE=2 # TODO: tune
+MICRO_BATCH_SIZE=4 # TODO: tune
 NUM_WORKERS=128 # TODO: tune
 
 composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoiser.py \
@@ -34,7 +35,7 @@ composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoi
   composer.optimizer.lr=${LR} \
   composer.optimizer.weight_decay=${WEIGHT_DECAY} \
   composer.algorithms.gradient_clipping.clipping_threshold=${GRAD_CLIP} \
-  composer.trainer.eval_interval="4ep" \
+  composer.trainer.eval_interval="1ep" \
   composer.trainer.max_duration=${MAX_DURATION} \
   composer.trainer.save_num_checkpoints_to_keep=1 \
   composer/lr_scheduler=cosine_annealing_with_warmup \
@@ -44,6 +45,7 @@ composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoi
   model.config.length=768 \
   model.config.backbone_config.keep_every_n_encoder_layers=${KEEP_EVERY_N_ENCODER_LAYERS} \
   model.config.backbone_config.keep_every_n_decoder_layers=${KEEP_EVERY_N_DECODER_LAYERS} \
+  model.config.backbone_config.keep_top_n_decoder_layers=${KEEP_TOP_N_DECODER_LAYERS} \
   model.config.backbone_config.tie_encoder_decoder_weights=true \
   model.config.backbone_config.use_encoder_causal_mask=${USE_ENCODER_CAUSAL_MASK} \
   training.global_batch_size=${BATCH_SIZE} \
@@ -53,6 +55,6 @@ composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoi
   block_size=${BLOCK_SIZE} \
   training.antithetic_sampling=false \
   hydra.run.dir=/home/ubuntu/runs/dllm-dev/${RUN_NAME} \
-  composer.trainer.save_interval="8ep" \
+  composer.trainer.save_interval="1ep" \
   composer.loggers.name=${RUN_NAME} \
   train_dataloader.num_workers=${NUM_WORKERS}
