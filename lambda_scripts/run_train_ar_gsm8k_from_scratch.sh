fef320aca8a2245e455ec751f659e7ec5e11e355
Yair Schiff <yairschiff@gmail.com>
2025-05-14 21:26:08 -0400

    Upadte sizes

--- lambda_scripts/run_train_ar_gsm8k_from_scratch.sh
+++ lambda_scripts/run_train_ar_gsm8k_from_scratch.sh
@@ -15,12 +15,12 @@ GRAD_CLIP=1.0 # 0.25, 0.5, 0.75, 1.0
 WEIGHT_DECAY=1e-5 # 1e-5, 1e-3
 MAX_DURATION="20000ba" # 20000ba, 10000ba, 5000ba
 
-PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-1.7B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
+PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-0.6B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
 
-TAG=ar_qweb2B_from_scratch_v1
+TAG=ar_qwen600m_from_scratch_v1
 RUN_NAME=gsm8k-bs${BATCH_SIZE}-keep${KEEP_EVERY_N_LAYERS}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
 
-MICRO_BATCH_SIZE=2 # TODO: tune
+MICRO_BATCH_SIZE=8 # TODO: tune
 NUM_WORKERS=64 # TODO: tune
 
 composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoiser.py \
--- lambda_scripts/run_train_e2d2_gsm8k_from_scratch.sh
+++ lambda_scripts/run_train_e2d2_gsm8k_from_scratch.sh
@@ -12,20 +12,20 @@ USE_ENCODER_CAUSAL_MASK=false # true, false
 KEEP_TOP_N_DECODER_LAYERS=14
 
 # Hyperparameters
-LR=1e-4 # 1e-5, 1e-4, 1e-3
+LR=3e-4 # 1e-5, 1e-4, 1e-3
 WARMUP_DURATION="1000ba" # 0.1, 0.3, 0.5
 BATCH_SIZE=96 # 96, 128, 256
 GRAD_CLIP=1.0 # 0.25, 0.5, 0.75, 1.0
 WEIGHT_DECAY=1e-5 # 1e-5, 1e-3
 MAX_DURATION="20000ba" # 20000ba, 10000ba, 5000ba
 
-PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-1.7B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
+PRETRAINED_MODEL_NAME_OR_PATH=Qwen/Qwen3-0.6B-Base # Qwen/Qwen3-0.6B-Base, Qwen/Qwen3-1.7B-Base, microsoft/Phi-4-mini-reasoning
 
-TAG=e2d2_qwen2B_tie_v1
+TAG=e2d2_qwen600m_tie_from_scratch_v1
 RUN_NAME=gsm8k-block${BLOCK_SIZE}-bs${BATCH_SIZE}-keeptop${KEEP_TOP_N_DECODER_LAYERS}-causalenc${USE_ENCODER_CAUSAL_MASK}-max${MAX_DURATION}-lr${LR}-warmup${WARMUP_DURATION}-gc${GRAD_CLIP}-wd${WEIGHT_DECAY}-${TAG}
 
-MICRO_BATCH_SIZE=4 # TODO: tune
-NUM_WORKERS=128 # TODO: tune
+MICRO_BATCH_SIZE=8 # TODO: tune
+NUM_WORKERS=64 # TODO: tune
 
 composer -n ${NUM_VISIBLE_DEVICES} scripts/composer_scripts/train_discrete_denoiser.py \
   run_name=${RUN_NAME} \
