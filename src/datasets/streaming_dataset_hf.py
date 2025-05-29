3751066bac8b9f89742706caf309bd0ad6be6769
Marianne Arriola <ma2238@g2-login-05.coecis.cornell.edu>
2025-05-29 15:58:20 -0400

    fix fsdp/hf streaming

--- src/datasets/streaming_dataset_hf.py
+++ src/datasets/streaming_dataset_hf.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 
+import torch.distributed as dist
 from torch.utils.data import IterableDataset
 from transformers import PreTrainedTokenizer
 
@@ -12,17 +13,22 @@ def load_streaming_dataset(
     tokenizer: PreTrainedTokenizer,
     shuffle_buffer_size: int,
     seed: int,
-    take_size: int,
     max_length: int,
+    take_size: int = 0,
     padding: bool = False,
     **_: Dict[str, Any],
 ):
     ds = load_dataset(name, split=split, streaming=True)
 
+    if dist.is_available() and dist.is_initialized():
+        num_shards = dist.get_world_size()
+        index = dist.get_rank()
+        ds = ds.shard(num_shards=num_shards, index=index)
+
     if shuffle_buffer_size:
         ds = ds.shuffle(buffer_size=shuffle_buffer_size, seed=seed)
 
-    if take_size:
+    if take_size > 0:
         ds = ds.take(take_size)
 
     columns_to_keep = ["input_ids", "attention_mask"]
