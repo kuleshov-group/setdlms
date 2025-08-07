117b149fcaff5bcf48324a60cc88d615d7c976e7
Yair Schiff <yairschiff@gmail.com>
2025-08-07 15:45:06 -0400

    Standardize hendryks math to gsm8k

--- src/datasets/tokenize_on_demand.py
+++ src/datasets/tokenize_on_demand.py
@@ -238,11 +238,20 @@ class GSM8KAugDataset(GSM8KDataset):
         }
 
 
-class HendrycksMathDataset(Dataset):
+class HendrycksMathDataset(GSM8KDataset):
     def __init__(
         self,
         tokenizer: PreTrainedTokenizer,
         split: Literal["train", "test"],
+        config_name: Literal[
+            "algebra",
+            "counting_and_probability",
+            "geometry",
+            "intermediate_algebra",
+            "number_theory",
+            "prealgebra",
+            "precalculus",
+        ],
         max_length: int,
         dataset_path: str = "EleutherAI/hendrycks_math",
         padding: bool = False,
@@ -251,68 +260,24 @@ class HendrycksMathDataset(Dataset):
         target_prompt_text: str | None = "Answer: ",
         source_key: str = "problem",
         target_key: str = "solution",
+        num_shot: int = 0,
         # Unused tokenizer arg (compat. with other dataset loading functions/classes)
         **_: Dict[str, Any],
     ):
-        self.tokenizer = tokenizer
-        self.dataset = load_dataset(dataset_path, split=split, trust_remote_code=True)
-        self.max_length = max_length
-        self.padding = padding
-        self.add_special_tokens = add_special_tokens
-        self.source_prompt_text = source_prompt_text
-        self.target_prompt_text = target_prompt_text
-        self.source_key = source_key
-        self.target_key = target_key
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        example = self.dataset[idx]
-        if self.source_prompt_text is not None:
-            example[self.source_key] = (
-                self.source_prompt_text + example[self.source_key]  # type: ignore
-            )
-        if self.target_prompt_text is not None:
-            example[self.target_key] = (
-                self.target_prompt_text + example[self.target_key]  # type: ignore
-            )
-        if self.add_special_tokens:
-            example[self.source_key] = (
-                self.tokenizer.bos_token
-                + example[self.source_key]
-                + self.tokenizer.eos_token
-            )
-            example[self.target_key] = (
-                example[self.target_key] + self.tokenizer.eos_token
-            )
-
-        qa_tokenized = self.tokenizer.batch_encode_plus(
-            [example[self.source_key], example[self.target_key]],
-            max_length=self.max_length // 2,
-            padding=self.padding,
-            add_special_tokens=False,  # (potentially) added manually, above
-            truncation=True,
+        super().__init__(
+            tokenizer=tokenizer,
+            split=split,
+            config_name=config_name,  # type: ignore
+            max_length=max_length,
+            dataset_path=dataset_path,
+            padding=padding,
+            add_special_tokens=add_special_tokens,
+            source_prompt_text=source_prompt_text,
+            target_prompt_text=target_prompt_text,
+            source_key=source_key,
+            target_key=target_key,
+            num_shot=num_shot,
         )
-
-        input_ids = torch.cat(
-            [torch.LongTensor(t) for t in qa_tokenized["input_ids"]], dim=-1
-        )
-        attention_mask = torch.cat(
-            [torch.LongTensor(a) for a in qa_tokenized["attention_mask"]], dim=-1
-        )
-        context_mask = torch.cat(
-            (
-                torch.LongTensor(qa_tokenized["attention_mask"][0]),
-                torch.zeros_like(torch.LongTensor(qa_tokenized["input_ids"][1])),
-            ),
-            dim=-1,
-        )
-        return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "context_mask": context_mask,
-        }
 
 
 class CNNDailyMailDataset(Dataset):
